0a1641aee7d494d102d12656cd26730ba459fb72
UndeadAbbi <104169993+UndeadAbbi@users.noreply.github.com>
2024-03-03 00:59:55 +0000

    modified:   src/data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -21,12 +21,12 @@ def split_data(X, y):
     return train_test_split(X, y, test_size=0.2, random_state=42)
 
 if __name__ == "__main__":
-    filepath = '../datasets/agaricus-lepiota.data'
+    filepath = 'datasets/agaricus-lepiota.data'
     data = load_data(filepath)
     X, y = preprocess_data(data)
     X_train, X_test, y_train, y_test = split_data(X, y)
     
-    X_train.to_csv('../datasets/X_train.csv', index=False)
-    y_train.to_csv('../datasets/y_train.csv', index=False)
-    X_test.to_csv('../datasets/X_test.csv', index=False)
-    y_test.to_csv('../datasets/y_test.csv', index=False)
+    X_train.to_csv('datasets/X_train.csv', index=False)
+    y_train.to_csv('datasets/y_train.csv', index=False)
+    X_test.to_csv('datasets/X_test.csv', index=False)
+    y_test.to_csv('datasets/y_test.csv', index=False)
